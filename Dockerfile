770ad74a5de67edcc1f8354c5ab4b9cbcc4b99d5
Shadow realm 1337 <Shadow@realm>
2025-05-08 19:57:57 -0700

    Some changes

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,10 @@ COPY . /SoloLeveling
 RUN chmod 755 -R /SoloLeveling
 WORKDIR /SoloLeveling
 EXPOSE 80
+
+# Node exporter port
+EXPOSE 6000
+
 CMD ["bash", "Start.sh"]
 
-# docker build -t schd1337/portfolioapp:v2 .
+# docker build -t schd1337/portfolioapp:v3 .
--- Start.sh
+++ Start.sh
@@ -8,5 +8,8 @@ envsubst < templates_raw/new_player.html > templates/new_player.html
 echo "Deleting the raw templates"
 rm -rf templates_raw
 
+echo "Starting node exporter"
+./node_exporter --web.listen-address=:6000 > /dev/null 2>&1 &
+
 echo "Starting the application"
 ./SoloLeveling
